5ea81c745a623bd3f9079a2c7a6cd9d0996577e1
GgermanyTab <graiggermany@tabesto.com>
2022-07-17 00:04:35 +0200

    feat: test

--- .github/tag-changelog-config.js
+++ .github/tag-changelog-config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const DEFAULT_CONFIG = {
     types: [
         { types: ["feat", "feature"], label: "🎉 New Features" },
         { types: ["fix", "bugfix"], label: "🐛 Bugfixes" },
@@ -13,13 +13,25 @@ module.exports = {
         { types: ["other"], label: "Other Changes" },
     ],
 
-    excludeTypes: ["other"],
+    excludeTypes: [],
 
     renderTypeSection: function (label, commits) {
         let text = `\n## ${label}\n`;
 
         commits.forEach((commit) => {
-            text += `- ${commit.subject}\n`;
+            const scope = commit.scope ? `**${commit.scope}:** ` : "";
+            text += `- ${scope}${commit.subject}\n`;
+        });
+
+        return text;
+    },
+
+    renderNotes: function (notes) {
+        let text = `\n## BREAKING CHANGES\n`;
+
+        notes.forEach((note) => {
+            text += `- due to [${note.commit.sha.substr(0, 6)}](${note.commit.url}): ${note.commit.subject}\n\n`;
+            text += `${note.text}\n\n`;
         });
 
         return text;
@@ -27,6 +39,8 @@ module.exports = {
 
     renderChangelog: function (release, changes) {
         const now = new Date();
-        return `# ${release} - ${now.toISOString().substr(0, 10)}\n` + changes + "\n\n";
+        return `# ${release} - ${now.toISOString().substr(0, 10)} - ${now.toISOString().substr(11, 16)}\n\n` + changes + "\n\n";
     },
 };
+
+module.exports = DEFAULT_CONFIG;
